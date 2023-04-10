14e443d9de4732a3811cbc32f3f3c60a8941c54c
NguyenThiNhuPhuong <nguyenthinhuphuong242@gmail.com>
2023-04-10 09:53:33 +0700

    # update

---
+++ backend/app/Models/OrderStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    use HasFactory;
+    protected $table="orders";
+    protected $fillable = [
+        'code',
+        'user_id',
+        'date',
+        'name',
+        'phone',
+        'email',
+        'price_product',
+        'price_ship',
+        'price_all',
+        'discount_id',
+        'province_id',
+        'district_id',
+        'ward_id',
+        'address',
+        'note',
+        'order_status_id',
+    ];
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'deleted_at'
+    ];
+}
---
+++ backend/app/Repositories/OrderDetailRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Categories;
+
+class CategoryRepository
+{
+
+    protected $modelClass = Categories::class;
+
+
+    public function getAllCategory()
+    {
+        return  $this->modelClass::all();
+    }
+
+    public function getCategory($categoryId)
+    {
+        return  $this->modelClass::find($categoryId);
+    }
+
+    public function createCategory($data)
+    {
+        return $this->modelClass::create($data);
+
+    }
+    public function updateCategory($id, $data)
+    {
+        $user = $this->modelClass::findOrFail($id);
+        $user->update($data);
+        return $user;
+    }
+
+    public function deleteCategory($id){
+        $user = $this->modelClass::findOrFail($id);
+        $user->delete();
+        return $user;
+    }
+}
