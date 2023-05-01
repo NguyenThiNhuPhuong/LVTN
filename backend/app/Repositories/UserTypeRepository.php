862b6e9e87ef84d6e9ad62d78ca47464029d8e9e
NguyenThiNhuPhuong <nguyenthinhuphuong242@gmail.com>
2023-05-02 02:04:47 +0700

    api users

---
+++ backend/app/Repositories/UserTypeRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Categories;
+use App\Models\User;
+
+class UserRepository
+{
+
+    protected $modelClass = User::class;
+
+
+    public function getAllUser()
+    {
+        return  $this->modelClass::all()->toArray();
+    }
+
+    public function getUser($userId)
+    {
+        return  $this->modelClass::find($userId)->toArray();
+    }
+
+    public function createUser($data)
+    {
+        return $this->modelClass::create($data);
+
+    }
+    public function updateUser($id, $data)
+    {
+        $user = $this->modelClass::findOrFail($id);
+        $user->update($data);
+        return $user;
+    }
+
+    public function deleteUser($id){
+        $user = $this->modelClass::findOrFail($id);
+        $user->delete();
+        return $user;
+    }
+}
---
+++ backend/app/Services/UserTypeService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\AddressRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    protected $addressRepository;
+    protected $userRepository;
+
+    public function __construct()
+    {
+        $this->addressRepository = new AddressRepository;
+        $this->userRepository = new UserRepository;
+        $this->userTypeRepository = new U;
+
+    }
+
+    public function repariDataUser($user)
+    {
+        $user['type_name'] = $this->userTypeRepository->getProvince($user['province_id'])->name ;
+        $user['province_name'] = $user['province_id'] ? $this->addressRepository->getProvince($user['province_id'])->name : null;
+        $user['district_name'] = $user['district_id'] ? $this->addressRepository->getDistrict($user['district_id'])->name : null;
+        $user['ward_name'] = $user['ward_id'] ? $this->addressRepository->getWard($user['ward_id'])->name : null;
+        return $user;
+    }
+
+    public function repariDataRequest($request,$action){
+        $user= [];
+        switch ($action) {
+            case 'add':
+                $user = [
+                    'name' => $request->name,
+                    'avatar' => "https://tse2.mm.bing.net/th?id=OIP.lF8ztkPyzv_NrpD7V8YYVAHaHa&pid=Api&P=0",
+                    'email' => $request->email,
+                    'type' => $request->type,
+                    'phone' => $request->phone,
+                    'password' => Hash::make($request->password),
+                    'province_id' => $request->province_id,
+                    'district_id' => $request->district_id,
+                    'ward_id' => $request->ward_id,
+                    'address' => $request->address,
+                    'created_by'=>Auth::user()->id,
+                    'updated_by'=>Auth::user()->id,
+                ];
+                break;
+            case 'update':
+                $user = [
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'active' => $request->active ,
+                    'updated_by'=>Auth::user()->id,
+                ];
+                break;
+
+            default:
+                break;
+        }
+        return  $user;
+
+    }
+
+    public function createUser($data)
+    {
+       return  $this->userRepository->createUser($data);
+    }
+
+}
