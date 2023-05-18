4a330f4daf6777e903c33602524c7e2a4cb20da8
Nabeo <phanthingocanh2001.ptna@gmail.com>
2023-05-18 23:03:51 +0700

    edit page account

--- frontend/src/redux/slice/auth/AuthSlice.jsx
+++ frontend/src/redux/slice/auth/AuthSlice.jsx
@@ -3,20 +3,12 @@ import * as registerService from "../../../services/registerService";
 import Cookies from "js-cookie";
 
 export const signUpUser = createAsyncThunk("user/signUpUser", async (user) => {
-  try {
-    const response = await registerService.signUpUser(user);
-    return response;
-  } catch (e) {
-    console.log(e);
-  }
+  const response = await registerService.signUpUser(user);
+  return response;
 });
 export const signInUser = createAsyncThunk("user/userLogin", async (user) => {
-  try {
-    const response = await registerService.signInUser(user);
-    return response.data;
-  } catch (e) {
-    console.log(e);
-  }
+  const response = await registerService.signInUser(user);
+  return response.data;
 });
 const userInfoFromLocalStorage = localStorage.getItem("userInfo")
   ? Cookies.get("token") && JSON.parse(localStorage.getItem("userInfo"))
@@ -32,7 +24,7 @@ const authSlice = createSlice({
     token: Cookies.get("token"),
     isOpenModal: false,
     email: "",
-    user: {},
+    user: { current_password: "", new_password: "", confirm_new_password: "" },
   },
   reducers: {
     logoutUser: (state) => {
